8f81a28719f0b0a36b75853168db5764bfdd51c7
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-09 07:21:37 +0300

    Create googleSimpleSearch.test.js

---
+++ googleSimpleSearch.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Import Testim Dev Kit methods
+const { go, text, test, l, Locator, resize, click, type } = require('testim');
+
+// Import chai assertion library
+const { expect } = require('chai');
+Locator.set(require('./locators/locators.js'));
+//go('www.google.com');
+test('My first text validation', async () => {
+  await go('google.com');
+  await resize({
+    width: 1024,
+    height: 768,
+  });
+  await type('.gLFyf', 'car');
+  await click('.gNO89b');
+}); // end of test
