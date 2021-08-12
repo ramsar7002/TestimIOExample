2bb3b755b237a56ed33d3d18e5264b4ef5d53347
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-12 18:40:33 +0300

    Update googleSimpleSearch.test.js

--- googleSimpleSearch.test.js
+++ googleSimpleSearch.test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// Import Testim Dev Kit methods
+
 const { go, text, test, l, Locator, resize, click, type } = require('testim');
 
-// Import chai assertion library
+
 const { expect } = require('chai');
 Locator.set(require('./locators/locators.js'));
 
@@ -15,4 +15,4 @@ test('My first text validation', async () => {
   });
   await type('.gLFyf', 'car');
   await click('.gNO89b');
-}); // end of test
+});
